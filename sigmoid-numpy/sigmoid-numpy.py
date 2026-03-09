45ecc56886c42976af987b6f0fba172683f0e5cb
Trần Đức Bảo <24020039@vnu.edu.vn>
2026-03-09 11:29:02 +0700

    Update sigmoid-numpy.py
    More instruction about code

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -5,5 +5,5 @@ def sigmoid(x):
     Vectorized sigmoid function.
     """
     # Write code here
-    x = np.asarray(x, dtype=float)
-    return 1 / (1 + np.exp(-x))
+    x = np.asarray(x, dtype=float) #Cast the input into NumPy Array to use np.exp()
+    return 1 / (1 + np.exp(-x)) #Sigmoid function: g(z) = 1 / (1 + e^-z)
