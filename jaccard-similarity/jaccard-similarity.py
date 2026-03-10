0c6f0c056275b4fbf4f214736e5ebc5270047569
Trần Đức Bảo <24020039@vnu.edu.vn>
2026-03-10 10:15:16 +0700

    Update solution for jaccard-similarity

--- jaccard-similarity/jaccard-similarity.py
+++ jaccard-similarity/jaccard-similarity.py
@@ -1,12 +1,9 @@
-import numpy as np
 def jaccard_similarity(set_a, set_b):
     """
     Compute the Jaccard similarity between two item sets.
     """
     # Write code here
-    unique_a = np.unique(set_a)
-    unique_b = np.unique(set_b)
-    a_and_b = np.intersect1d(unique_a, unique_b)
-    a_or_b = np.union1d(unique_a, unique_b)
-    if a_or_b.size == 0: return 0.0
-    return a_and_b.size / a_or_b.size
+    a = set(set_a)
+    b = set(set_b)
+    if len(a.union(b)) == 0: return 0.0
+    return len(a.intersection(b)) / len(a.union(b))
